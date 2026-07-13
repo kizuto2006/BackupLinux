e3ef81c527f56ab57e9048a1ab3b45d95c521c2b
kizuto2006 <vhung7986@gmail.com>
2026-07-13 14:10:40 +0700

    add NixOS config can build

---
+++ nixos/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia/legacy-v4";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, noctalia, ... }:
+  {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./hosts/acer-a715/configuration.nix
+        home-manager.nixosModules.home-manager
+
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.kizuto = {
+            home.stateVersion = "26.05";
+
+            home.packages = [
+              noctalia.packages.x86_64-linux.default
+            ];
+          };
+        }
+      ];
+    };
+  };
+}
